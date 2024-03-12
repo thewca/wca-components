48b14a40144a0806c922e80a55e2c697fb923061
FinnIckler <finnickler@gmail.com>
2024-03-12 18:21:22 +0100

    build separate css files

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const { build } = require('esbuild')
 const { sassPlugin } = require('esbuild-sass-plugin')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
+const { globSync } = require('glob')
 
 const entryFile = 'src/index.ts'
 const shared = {
@@ -20,6 +21,15 @@ build({
   format: 'esm',
   outfile: './dist/index.esm.js',
   target: ['esnext', 'node12.22.0'],
+})
+
+// Build separate css files to be able to only include one component
+build({
+  ...shared,
+  format: 'esm',
+  outdir: './dist/css/',
+  target: ['esnext', 'node12.22.0'],
+  entryPoints: globSync('./src/components/**/index.ts'),
 })
 
 build({
